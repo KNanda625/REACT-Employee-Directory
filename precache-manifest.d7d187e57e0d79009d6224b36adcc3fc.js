4fa6db3501d906258deeae65ae6668df376d498b
Kauri Nanda <bknanda6@gmail.com>
2020-05-30 23:18:15 -0600

    Updates

---
+++ precache-manifest.d7d187e57e0d79009d6224b36adcc3fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed36244263aaa277e795d83b3d10c66c",
+    "url": "/REACT-Employee-Directory/index.html"
+  },
+  {
+    "revision": "56e1b07a12808a63e368",
+    "url": "/REACT-Employee-Directory/static/css/main.bba858e1.chunk.css"
+  },
+  {
+    "revision": "c194b42cee4233c6a353",
+    "url": "/REACT-Employee-Directory/static/js/2.55eaa146.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/REACT-Employee-Directory/static/js/2.55eaa146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56e1b07a12808a63e368",
+    "url": "/REACT-Employee-Directory/static/js/main.73297c7c.chunk.js"
+  },
+  {
+    "revision": "b3091ab2a719da3c4f56",
+    "url": "/REACT-Employee-Directory/static/js/runtime-main.48d5dc95.js"
+  }
+]);
